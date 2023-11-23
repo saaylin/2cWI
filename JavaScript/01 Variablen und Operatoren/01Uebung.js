0b4e7ac34b925f20a6d6a41c52e310fd4422591d
Aylin <aylin@htldornbirn.at>
2023-11-23 09:31:56 +0100

    01Uebung.js End of lesson

---
+++ JavaScript/01 Variablen und Operatoren/01Uebung.js
@@ -0,0 +1,33 @@
+console.log("hello");
+console.log("Hallo");
+
+// Variablen 
+let a =7;
+let b =8;
+let c =10;
+
+
+console.log ("A hat den Wert" + a);
+console.log ("a+b=" + c);
+
+// Rechnungsarten 
+//*,/,+,-
+
+
+let eins = a+b+c;
+console.log("das ergebnis ist" + eins); 
+
+let zwei = (a+b)/c ;
+console.log("das ergebnis ist" + zwei); 
+
+let drei = c-a+b;
+console.log("das ergebnis ist" + drei); 
+
+let vier =  c -a + b ;
+console.log("das ergebnis ist" + vier); 
+
+let fünf = a + b + c * 3 ;
+console.log("das ergebnis ist" + fünf); 
+
+let sechs = (a + b + c)/4;
+console.log("das ergebnis ist" + sechs); 
