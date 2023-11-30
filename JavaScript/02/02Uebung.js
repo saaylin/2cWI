de2fde38ac942166e35d74eda37b292152f1a667
Aylin <aylin@htldornbirn.at>
2023-11-30 09:27:46 +0100

    03_Aufgabe/ 02Uebung finished

---
+++ JavaScript/02/02Uebung.js
@@ -0,0 +1,32 @@
+let a= 7;
+let b= 8;
+
+let result  = a-b;
+
+console.log(result);
+console.log(result);
+
+// +,-,*,/
+let c= 7.4;
+let d= 7.5;
+
+result1= c+d;
+console.log(result1);
+
+let e= 17.4
+let f= 3.9
+
+result2= e/f
+console.log(result2)
+
+// Texte STRINGS
+
+let name = "Aylino";
+console.log(name);
+
+console.log("the result of " +a+ "  minus  " +b+ "  ist  " +result);
+
+console.log("the result of  " +c+ " and " +d+ " is " + result1);
+
+console.log("the result of " +e+ " divided by " +f+ " is " + result2 );
+
---
+++ JavaScript/03 Aufgabe/03_Aufgabe.js
@@ -0,0 +1,29 @@
+// Preis Pro Kilo 
+let bananaPricePerKilo = 2.14
+let applePricePerKilo = 3.34 
+
+// durchschnittliches gewicht  
+let appleKilo = 0.34
+let bananaKilo = 0.22 
+
+
+// 8 Äpfel
+let anzahlapple = 8 
+result = anzahlapple * applePricePerKilo * appleKilo 
+console.log("8 Äpfel kosten " + result )
+
+// 17 bananen
+let anzahlbanana = 17
+result1 = anzahlbanana* bananaPricePerKilo * bananaKilo
+console.log ("17 Banana kosten " + result1)
+
+// 1 tonne Äpfel
+let gewichtApple = 1000 
+result2 = gewichtApple* applePricePerKilo
+console.log("1 tonne Äpfel kosten " + result2)
+
+
+// 1 tonne bananen
+let gewichtBanana = 1000
+result3 =  bananaPricePerKilo * gewichtBanana
+console.log("1 tonne Bananen Kosten " + result)
