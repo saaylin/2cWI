bf9ff7e90c74f758cd031f739a74d84c6b35474b
Aylin <aylin@htldornbirn.at>
2023-12-07 09:30:15 +0100

    04_loops / end of lesson

---
+++ JavaScript/04 Schleifen/04a_loops.js
@@ -0,0 +1,8 @@
+console.log("test");
+
+let a = 7;
+let b = 4.3;
+let name = "Aylino"; // Text
+
+console.log(name + " " + b + a);
+
---
+++ JavaScript/04 Schleifen/04b_loops.js
@@ -0,0 +1,29 @@
+console.log("text");
+
+//schleifen
+
+for(let count = 10; count<0; count--){
+    console.log("Aylino")
+}
+
+console.log("fertig")
+
+
+
+
+// Zähle von 7 bis 45 
+for(let count = 7; count<45; count++){
+    console.log("Aylino")
+}
+
+
+// Zähle von 9 bis 99 
+
+// Zähle rückwärts von 10 bis 0 
+
+// Zähle rückwärts von 1234 bis 1207 
+
+// Gib Deinen Namen genau 8 mal aus 
+
+// Zähle von 0 auf 10, dann gib Deinen Namen aus, dann zähle  
+
