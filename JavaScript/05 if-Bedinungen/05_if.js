dcdb6a1a98e7e0b40bb590b693b28a479d1c4218
Aylin <aylin@htldornbirn.at>
2023-12-14 09:29:04 +0100

    05_if.js end of lesson

---
+++ JavaScript/05 if-Bedinungen/05_if.js
@@ -0,0 +1,47 @@
+
+let number = 27;
+
+if (number<25){
+    console.log("number");
+} else {
+    console.log("else" + number)
+}
+
+// Aufgabe 1 
+// Wenn die Zahl kleiner ist als 20  gib aus "Mini" 
+number = 15;
+if(number<20){
+    console.log("mini")
+}
+
+// Wenn die Zahl zw. 20 und 50 ist gib aus "Medium" 
+zahl = 30 ;
+if (zahl >= 20 && zahl <= 50) {
+    console.log("Medium");
+} 
+
+// Wenn die Zahl größer als 50 ist gib aus "Large" 
+zahl = 60;
+if(zahl > 50){
+    console.log("Large");
+}
+
+
+// Aufgabe 2 
+// Wenn die Zahl kleiner gleich 7 ist gib aus "Mini" 
+number = 7;
+if(number <= 7){
+    console.log("Mini");
+}
+
+// Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium" 
+number = 50
+if(number >= 7 && number <= 90){
+    console.log("Medium");
+}
+
+// Wenn die Zahl größer als 90 ist gib aus "Large" 
+number = 1907
+if (number >=90){
+    console.log("Large");
+}
