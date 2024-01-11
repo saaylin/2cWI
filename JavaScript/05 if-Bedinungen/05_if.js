81ff21c2b602b9e48d10fbb2f3a73603579d11ee
Aylin <aylin@htldornbirn.at>
2024-01-11 08:52:55 +0100

    05 if bedinungen finished

--- JavaScript/05 if-Bedinungen/05_if.js
+++ JavaScript/05 if-Bedinungen/05_if.js
@@ -17,31 +17,31 @@ if(number<20){
 // Wenn die Zahl zw. 20 und 50 ist gib aus "Medium" 
 zahl = 30 ;
 if (zahl >= 20 && zahl <= 50) {
-    console.log("Medium");
+    console.log("medium");
 } 
 
 // Wenn die Zahl größer als 50 ist gib aus "Large" 
 zahl = 60;
 if(zahl > 50){
-    console.log("Large");
+    console.log("large");
 }
 
 
 // Aufgabe 2 
 // Wenn die Zahl kleiner gleich 7 ist gib aus "Mini" 
-number = 7;
-if(number <= 7){
-    console.log("Mini");
+zahl = 5 
+if (zahl < 7){
+    console.log("klein");
 }
 
 // Wenn die Zahl zwischen 7 und 90 ist gib  aus "Medium" 
-number = 50
-if(number >= 7 && number <= 90){
-    console.log("Medium");
+zahl = 60 
+if (zahl >= 7 && zahl <= 90){
+    console.log("mittel");
 }
 
 // Wenn die Zahl größer als 90 ist gib aus "Large" 
-number = 1907
-if (number >=90){
-    console.log("Large");
+zahl = 99
+if( zahl > 90){
+    console.log("groß"); 
 }
