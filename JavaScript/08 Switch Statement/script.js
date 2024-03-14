01f24127f82d6b717b5d6891aaf82ffb1fe7119d
Aylin <aylin@htldornbirn.at>
2024-03-14 08:04:14 +0100

    09 Aufgabe Zeichen not finished

--- JavaScript/08 Switch Statement/script.js
+++ JavaScript/08 Switch Statement/script.js
@@ -7,23 +7,23 @@ console.log("Zufallszahl: " + zufallszahl);
 // Verwende eine switch-Anweisung, um den passenden Text auszugeben
 switch (zufallszahl) {
     case 10:
-        console.log("Ten");
+        console.log("ten");
         break;
     case 9:
-        console.log("Nine");
+        console.log("nine");
         break;
     case 8:
-        console.log("Eight");
+        console.log("eight");
         break;
     case 7:
-        console.log("Seven");
+        console.log("seven");
         break;
     case 6:
-        console.log("Six");
+        console.log("six");
         break;
     case 5:
-        console.log("Five");
+        console.log("five");
         break;
     default:
-        console.log("Die Zufallszahl liegt außerhalb des Bereichs 5-10");
+        console.log("Die Zufallszahl liegt außerhalb im bereich von 5-10");
 }
