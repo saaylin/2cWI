55fa83f5620c24c4a8eaba6823ee4a7b3856d911
Aylin <aylin@htldornbirn.at>
2024-04-04 09:28:21 +0200

    10 Funktion end of lesson

---
+++ JavaScript/10 Funktionen/script.js
@@ -0,0 +1,8 @@
+/*Baue folgende Funktionen: 
+add(a,b) 
+subtract(a,b) 
+mulitply(a,b) 
+supercalculation(a,b) -> Formel: (a+b)/2 * a 
+printEasterDate(year) -> Diese Funktion bekommt das gewünschte Jahr und schreibt das Datum von Ostern. 
+Anbei ist die Formel zur Berechnung on Ostern. */ 
+
