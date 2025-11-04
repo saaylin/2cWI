694b2066b2cc2ec27f835de7942fb2b2e6f2819f
Aylin <aylin@htldornbirn.at>
2025-11-04 10:01:52 +0100

    push

--- JavaScript/11 Arrays/aufgabe2.js
+++ JavaScript/11 Arrays/aufgabe2.js
@@ -4,3 +4,23 @@ Gib folgenden Satz aus:
 "Meine Freunde sind Susi, Paula und Hans 
 Füge "Sepp" dazu. Nun soll die ausgabe folgendermaßen lauten: 
 "Meine Freunde sind Susi, Paula, Hans und Sepp" */
+
+
+let freunde = ["Susi", "Paula", "Hans"];
+
+
+function gibFreundeAus(freundeArray) {
+    let satz;
+    if (freundeArray.length > 1) {
+        satz = "Meine Freunde sind " + freundeArray.slice(0, -1).join(", ") + " und " + freundeArray[freundeArray.length - 1];
+    } else {
+        satz = "Mein Freund ist " + freundeArray[0];
+    }
+    console.log(satz);
+}
+
+gibFreundeAus(freunde);
+
+freunde.push("Sepp");
+
+gibFreundeAus(freunde);
